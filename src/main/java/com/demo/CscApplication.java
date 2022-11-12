1c7a612b1118e900a116c02598b16410915d3f95
Gopalraj Mishra <gopalrajmishra278@gmail.com>
2022-11-12 21:04:09 +0530

    Take a Map Cache in main method

--- src/main/java/com/demo/CscApplication.java
+++ src/main/java/com/demo/CscApplication.java
@@ -6,9 +6,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CscApplication {
 
+        private Map<String,Object> cache = new HashMap<String,Object>();
+
 	public static void main(String[] args) {
 		SpringApplication.run(CscApplication.class, args);
 	}
+
+
+        public void loadDataToCache(){
+          
+           //logic
+         }
         
         //Custmer Details
        public void doCustInfo(){
