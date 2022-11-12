e7f80d9fc02ea7c5669c7b6f6f12a4c2d2723ec2
Gopalraj Mishra <gopalrajmishra278@gmail.com>
2022-11-12 20:35:39 +0530

    variable declared in main method

--- src/main/java/com/demo/CscApplication.java
+++ src/main/java/com/demo/CscApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CscApplication {
 
+         public static final String Rest_Url="www.equifax.com/getScores";
+
 	public static void main(String[] args) {
 		SpringApplication.run(CscApplication.class, args);
 	}
